765b4541d1ca63aee5e4583e89e8c59613c79d2e
QuyenNguyen230698 <166916954+QuyenNguyen230698@users.noreply.github.com>
2024-07-23 20:26:31 +0700

    update Quyen

---
+++ js/index.js
@@ -0,0 +1,30 @@
+function domID(id) {
+  return document.getElementById(id);
+}
+
+var DSSV = [];
+
+function themSV() {
+  var ma = domID("txtMaSV").value;
+  var ten = domID("txtTenSV").value;
+  var email = domID("txtEmail").value;
+  var matKhau = domID("txtPass").value;
+  var diemToan = domID("txtDiemToan").value * 1;
+  var diemLy = domID("txtDiemLy").value * 1;
+  var diemHoa = domID("txtDiemHoa").value * 1;
+
+  var sv = {
+    ma: ma,
+    ten: ten,
+    email: email,
+    matKhau: matKhau,
+    diemToan: diemToan,
+    diemLy: diemLy,
+    diemHoa: diemHoa,
+    tinhDTB: function () {
+      return (this.diemToan + this.diemLy + this.diemHoa) / 3;
+    },
+  };
+  DSSV.push(sv);
+  console.log(DSSV);
+}
